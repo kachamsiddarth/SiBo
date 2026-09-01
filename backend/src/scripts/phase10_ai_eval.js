5cc6eb13600fc174c3767cf4ab1a6e0fcd3ba7cf
Siddarth <siddarth.191300kacham@gmail.com>
2026-09-02 00:52:39 +0530

    Some minor changes in evalaution

--- backend/src/scripts/phase10_ai_eval.js
+++ backend/src/scripts/phase10_ai_eval.js
@@ -14,6 +14,7 @@ import { v4 as uuidv4 } from 'uuid';
 /**
  * Known exception scenarios with expected investigation outcomes
  */
+
 const EXCEPTION_SCENARIOS = [
   {
     name: 'Simple Amount Mismatch',
